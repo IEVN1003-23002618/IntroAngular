0efb99214d881e560344bff46779413ad897cd41
Liz Ponce <86574@alumnos.utleon.edu.mx>
2026-09-11 18:54:17 -0600

    Forms Module 11/09/2026

--- src/app/heroes/heroes-list/heroes-list.ts
+++ src/app/heroes/heroes-list/heroes-list.ts
@@ -6,4 +6,46 @@ import { Component } from '@angular/core';
   styleUrl: './heroes-list.css',
   templateUrl: './heroes-list.html',
 })
-export class HeroesList {}
+export class HeroesList {
+
+  imageWidth:number=40;
+  imageMargin:number=2;
+  muestraImagen:boolean=true;
+  listFilter:string='';
+  
+  showImage():void{
+    this.muestraImagen=!this.muestraImagen;
+  }
+
+  heroes:any[]=[
+    {
+      imagen:"https://dragonball-api.com/characters/goku_normal.webp",
+      nombre:'Goku',
+      descripcion:'kame hame Ha',
+      race:'Saiyan',
+      ki:9000
+    },
+    {
+      imagen:"https://dragonball-api.com/characters/vegeta_normal.webp",
+      nombre:'Vegeta',
+      descripcion:'Final Flash',
+      race:'Saiyan',
+      ki:8500
+    },
+    {
+      imagen:"https://dragonball-api.com/characters/picolo_normal.webp",
+      nombre:'Piccolo',
+      descripcion:'Makankosappo',
+      race:'Namekiano',
+      ki:7000
+    },
+    {
+      imagen:"https://dragonball-api.com/characters/Freezer.webp",
+      nombre:'Freezer',
+      descripcion:'Death Ball',
+      race:'Alien',
+      ki:9500
+    },
+
+  ]
+}
